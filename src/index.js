24cdf9dedb6e9d0c6898caed9eb6a1388444a9f1
danilobrinu <ddbn.c2@gmail.com>
2019-01-10 22:09:45 -0500

    Add index file

--- src/index.js
+++ src/index.js
@@ -1,10 +1,17 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
+import { render } from 'react-dom';
+import { IconSettings, Settings } from '@salesforce/design-system-react';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+Settings.setAppElement('body');
+
+render(
+  <IconSettings iconPath="/assets/icons">
+    <App />
+  </IconSettings>,
+  document.getElementById('root')
+);
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
