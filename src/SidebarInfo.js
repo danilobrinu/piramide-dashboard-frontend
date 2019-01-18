fb601d21126c33efeec7b91c992486cc3b07024e
danilobrinu <ddbn.c2@gmail.com>
2019-01-17 19:10:02 -0500

    Update SidebarInfo component

--- src/SidebarInfo.js
+++ src/SidebarInfo.js
@@ -1,10 +1,33 @@
 import React, { Component } from 'react';
+import lodash from 'lodash';
+import moment from 'moment';
+import currency from 'currency.js';
 import classNames from 'classnames';
 import { Button } from '@salesforce/design-system-react';
 
 export default class SidebarInfo extends Component {
   render() {
-    const { showSidebarInfo, setShowSidebarInfo } = this.props;
+    const {
+      showSidebarInfo,
+      setShowSidebarInfo,
+      purchaseOrder,
+      orderType,
+      requester,
+      receiver,
+      paymentCondition,
+      shippingCondition,
+      reasonTransfer,
+      deliveryDate,
+      transportDate,
+      // Keep this code - Next Implementation
+      // advancePaymentSelected,
+      products,
+    } = this.props;
+
+    const subTotal = lodash.reduce(products, (acc, product) => acc + product.amount, 0);
+    const igv = subTotal * .18;
+    const total = subTotal + igv;
+    const peso = lodash.reduce(products, (acc, product) => acc + product.weight, 0);
 
     return (
       <div
@@ -34,7 +57,152 @@ export default class SidebarInfo extends Component {
               className="slds-tile__title slds-text-heading_small slds-truncate"
               title="Salesforce UX"
             >
-              <a href="#top">LA VIGA SAC</a>
+              <a href="#top">Datos Generales</a>
+            </h2>
+            <div className="slds-tile__detail">
+              <dl className="slds-list_horizontal slds-wrap">
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Valor de venta"
+                >
+                  Ordén de Compra:
+                </dt>
+
+                <dd className="slds-item_detail">{purchaseOrder}</dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="IGV"
+                >
+                  Clase de Pedido:
+                </dt>
+                <dd className="slds-item_detail">
+                  {orderType.length ? orderType[0].label : ''}
+                </dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Total de venta"
+                >
+                  Solicitante:
+                </dt>
+                <dd className="slds-item_detail">
+                  {requester.length ? requester[0].label : ''}
+                </dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Total de venta"
+                >
+                  Destinatario:
+                </dt>
+                <dd className="slds-item_detail">
+                  {receiver.length ? receiver[0].label : ''}
+                </dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Total de venta"
+                >
+                  Condición de pago:
+                </dt>
+                <dd className="slds-item_detail">
+                  {paymentCondition.length ? paymentCondition[0].label : ''}
+                </dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Total de venta"
+                >
+                  Fecha de entrega:
+                </dt>
+                <dd className="slds-item_detail">
+                  {deliveryDate ? moment(deliveryDate).format('DD/MM/YYYY') : ''}
+                </dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Total de venta"
+                >
+                  Condición de expedición:
+                </dt>
+                <dd className="slds-item_detail">
+                  {shippingCondition.length ? shippingCondition[0].label : ''}
+                </dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Total de venta"
+                >
+                  Motivo de traslado:
+                </dt>
+                <dd className="slds-item_detail">
+                  {reasonTransfer.length ? reasonTransfer[0].label : ''}
+                </dd>
+                <dt
+                  className="slds-item_label slds-text-color_weak"
+                  title="Total de venta"
+                >
+                 Cita de transporte:
+                </dt>
+                <dd className="slds-item_detail">
+                  {transportDate ? moment(transportDate).format('DD/MM/YYYY') : ''}
+                </dd>
+              </dl>
+            </div>
+          </article>
+          {/* Keep this code - Next Implementation */}
+          {/* <article className="slds-tile">
+            <h2
+              className="slds-tile__title slds-text-heading_small slds-truncate"
+              title="Salesforce UX"
+            >
+              <a href="#top">Anticipo</a>
+            </h2>
+            {advancePaymentSelected.length ? (
+              <div className="slds-tile__detail">
+                <dl className="slds-list_horizontal slds-wrap">
+                  <dt
+                    className="slds-item_label slds-text-color_weak"
+                    title="Valor de venta"
+                  >
+                    Serie:
+                  </dt>
+                  <dd className="slds-item_detail">
+                    {advancePaymentSelected[0].serie}
+                  </dd>
+                  <dt
+                    className="slds-item_label slds-text-color_weak"
+                    title="IGV"
+                  >
+                    Importe:
+                  </dt>
+                  <dd className="slds-item_detail">
+                    {advancePaymentSelected[0].amount}
+                  </dd>
+                  <dt
+                    className="slds-item_label slds-text-color_weak"
+                    title="Total de venta"
+                  >
+                    Saldo:
+                  </dt>
+                  <dd className="slds-item_detail">
+                    {advancePaymentSelected[0].balance}
+                  </dd>
+                  <dt
+                    className="slds-item_label slds-text-color_weak"
+                    title="Total de venta"
+                  >
+                    Fecha:
+                  </dt>
+                  <dd className="slds-item_detail">
+                    {advancePaymentSelected[0].date}
+                  </dd>
+                </dl>
+              </div>
+            ) : (
+              <div>No se ha seleccionado ningún anticipo</div>
+            )}
+          </article> */}
+          <article className="slds-tile">
+            <h2
+              className="slds-tile__title slds-text-heading_small slds-truncate"
+              title="Salesforce UX"
+            >
+              <a href="#top">Pedido</a>
             </h2>
             <div className="slds-tile__detail">
               <dl className="slds-list_horizontal slds-wrap">
@@ -44,28 +212,28 @@ export default class SidebarInfo extends Component {
                 >
                   Subtotal:
                 </dt>
-                <dd className="slds-item_detail">6,850.00</dd>
+                <dd className="slds-item_detail">{currency(subTotal).format()}</dd>
                 <dt
                   className="slds-item_label slds-text-color_weak"
                   title="IGV"
                 >
                   IGV:
                 </dt>
-                <dd className="slds-item_detail">1,233.00</dd>
+                <dd className="slds-item_detail">{currency(igv).format()}</dd>
                 <dt
                   className="slds-item_label slds-text-color_weak"
                   title="Total de venta"
                 >
                   Total:
                 </dt>
-                <dd className="slds-item_detail">8,083.00</dd>
+                <dd className="slds-item_detail">{currency(total).format()}</dd>
                 <dt
                   className="slds-item_label slds-text-color_weak"
                   title="Total de venta"
                 >
                   Peso:
                 </dt>
-                <dd className="slds-item_detail">2 TONELADAS</dd>
+                <dd className="slds-item_detail">{peso}</dd>
               </dl>
             </div>
           </article>
