8245ee00b7e995b92129c15a58711bb18b2086e3
danilobrinu <ddbn.c2@gmail.com>
2019-01-17 19:09:16 -0500

    Update AddProductModal component

--- src/AddProductModal.js
+++ src/AddProductModal.js
@@ -6,99 +6,54 @@ import {
   comboboxFilterAndLimit,
   Input,
 } from '@salesforce/design-system-react';
-
-const uid = () => (+new Date() * Math.random()).toString(16).replace('.', '');
-
-const MockProductList = [
-  {
-    id: uid(),
-    label: 'Ladrillo King King 15H',
-    code: '04-0000033',
-    // quantity: 8000,
-    // weight: 4000,
-    // unity: 'UN',
-    // amount: 2500,
-  },
-  {
-    id: uid(),
-    label: 'Ladrillo Caravista',
-    code: '04-0000200',
-    // quantity: 3000,
-    // weight: 5000,
-    // unity: 'UN',
-    // amount: 4200,
-  },
-  {
-    id: uid(),
-    label: 'Ladrillo Caravista',
-    code: '04-0000200',
-    // quantity: 3000,
-    // weight: 5000,
-    // unity: 'UN',
-    // amount: 4200,
-  },
-  // {
-  //   id: 'd7679cdd',
-  //   name: 'Acme - 800 Widgets',
-  //   code: 'Acme',
-  //   quantity: '6/11/18',
-  //   unity: 'Value Proposition',
-  //   confidence: '85%',
-  //   amount: '$970,000',
-  //   contact: 'jrogers@acme.com',
-  // },
-];
+import { uid, optionsWithIcon } from './utils/helpers'
 
 export default class AddProductModal extends Component {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      productValue: '',
-      products: MockProductList,
-      product: [],
-      quantity: 1,
-    };
-
-    this.setQuantity = this.setQuantity.bind(this);
-    this.setProduct = this.setProduct.bind(this);
-  }
-
-  setQuantity(_, { number: quantity = 1 }) {
-    this.setState({ quantity });
-  }
-
-  setProduct(_, { value: productValue = '', selection: product = [] }) {
-    this.setState({ productValue, product });
-  }
-
   cancelOperation(e) {
-    this.setShowAddProductModal(e, { value: false });
+    const { setShowAddProductModal } = this.props;
+    setShowAddProductModal(e, { value: false });
   }
 
   saveOperation(e) {
-    if (!this.state.product.length) return;
+    if (!this.props.material.length) return;
 
-    const { setShowAddProductModal, addProductToOrder } = this.props;
-    const { product: productSelection, quantity } = this.state;
+    const {
+      setShowAddProductModal,
+      addProductToOrder,
+      material,
+      setMaterial,
+      materialQuantity: quantity,
+      setMaterialQuantity,
+      setEnabledOrder,
+    } = this.props;
+
     const product = {
       id: uid(),
-      name: productSelection[0].label,
-      code: productSelection[0].code,
+      name: material[0].label,
+      code: material[0].code,
       weight: 0,
       unity: 'UN',
       amount: 0,
       quantity,
     };
 
-    this.setProduct(e, {});
-    this.setQuantity(e, {});
+    setMaterial(e, {});
+    setMaterialQuantity(e, {});
     addProductToOrder(e, { product });
     setShowAddProductModal(e, { value: false });
+    setEnabledOrder(e, { value: false });
   }
 
   render() {
-    const { showAddProductModal } = this.props;
+    const {
+      showAddProductModal,
+      materialList,
+      materialValue,
+      material,
+      setMaterial,
+      materialQuantity,
+      setMaterialQuantity,
+    } = this.props;
 
     return (
       <Modal
@@ -122,24 +77,25 @@ export default class AddProductModal extends Component {
               <div className="slds-form-element__row">
                 <div className="slds-size_1-of-1">
                   <Combobox
-                    id="product"
+                    id="material"
                     events={{
-                      onChange: (e, { value }) => this.setProduct(e, { value }),
-                      onRequestRemoveSelectedOption: (e, _) => this.setProduct(e, {}),
-                      onSelect: (e, { selection }) => this.setProduct(e, { selection }),
+                      onChange: (e, { value }) => setMaterial(e, { value }),
+                      onRequestRemoveSelectedOption: (e, _) => setMaterial(e, {}),
+                      onSelect: (e, { selection }) => setMaterial(e, { selection }),
                     }}
                     labels={{
                       label: 'Producto',
                       placeholder: 'Buscar Producto',
                     }}
                     options={comboboxFilterAndLimit({
-                      inputValue: this.state.productValue,
-                      options: this.state.products,
-                      selection: this.state.product,
+                      inputValue: materialValue,
+                      options: optionsWithIcon(materialList),
+                      selection: material,
                     })}
-                    selection={this.state.product}
-                    value={this.state.productValue}
+                    selection={material}
+                    value={materialValue}
                     variant="inline-listbox"
+                    menuPosition="relative"
                     required
                   />
                 </div>
@@ -151,9 +107,9 @@ export default class AddProductModal extends Component {
                     label="Cantidad"
                     minValue={1}
                     maxValue={30000}
-                    onChange={(e, data) => this.setQuantity(e, data)}
+                    onChange={(e, data) => setMaterialQuantity(e, data)}
                     step={1}
-                    value={this.state.quantity}
+                    value={materialQuantity}
                     variant="counter"
                     required
                   />
